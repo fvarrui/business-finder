7111720cad3b544ffa4a8dc8fd6094ba687c7ccd
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2025-02-10 23:53:48 +0000

    Update __init__.py

--- src/bf/__init__.py
+++ src/bf/__init__.py
@@ -6,6 +6,6 @@ load_dotenv()
 # init global variables
 __module__ = 'bf'
 __project_name__ = "business-finder"
-__project_version__ = "0.0.1"
-__project_description__ = "Buscador de empresas"
+__project_version__ = "0.1.0"
+__project_description__ = "Buscador de empresas chupiguay"
 __project_url__ = "https://github.com/fvarrui/business-finder"
